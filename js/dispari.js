0b81a05b49dc99a69a8427b5b8ac424972647bf1
Luca1882 <luca.notarfranco@gmail.com>
2025-01-09 00:29:22 +0100

    Fix: Corretto esercizio Pari e dispari

--- js/dispari.js
+++ js/dispari.js
@@ -1,45 +1,39 @@
 //Funzione Numero casuale
 function numeroCasuale() {
     return Math.floor(Math.random() * 5) + 1;
-}
+};
 
 //Funzione isPari
 function isPari(somma) {
-    return somma % 2 === 0 ? "pari" : "dispari";
+    return somma % 2 === 0 ? "PARI" : "DISPARI";
 }
-
-//Funzione per determinare il vincitore
-function vincitore(giocatoreUno, giocatoreDue, risultato, sceltaUno, sceltaDue) {
-    if (risultato === "pari" && sceltaUno === "PARI") {
-        return "Il Giocatore Uno ha vinto la partita";
-    } else if (risultato === "dispari" && sceltaUno === "DISPARI") {
-        return "Il Giocatore Uno ha vinto la partita";
-    } else {
-        return "Il Giocatore Due ha vinto la partita";
-    }
-};
 
 //Funzione Inizio Partita
 function playTheGame() {
     //Scelta "Pari o Dispari" dei giocatori
-    let giocatoreUno = prompt("Giocatore Uno scegli pari o dispari: ").toUpperCase();
-    let giocatoreDue = prompt("Giocatore Due scegli pari o dispari: ").toUpperCase();
+    let sceltaGiocatore = prompt("Giocatore Uno scegli pari o dispari: ").toUpperCase();
+
 
     //Scelta del numero
-    let numGiocatoreUno = parseInt(prompt("Giocatore Uno scegli un numero da 1 a 5: "));
-    let numGiocatoreDue = parseInt(prompt("Giocatore Due scegli un numero da 1 a 5: "));
+    let numGiocatore = parseInt(prompt("Giocatore Uno scegli un numero da 1 a 5: "));
+    let numCom = numeroCasuale();
 
     //Somma dei numeri per determinare il vincitore e confronto
-    let somma = numGiocatoreUno + numGiocatoreDue;
+    let somma = numGiocatore + numCom;
     let verifica = isPari(somma);
-
-    // Determina chi vince
-    let resVincitore = vincitore(giocatoreUno, giocatoreDue, verifica, giocatoreUno, giocatoreDue);
 
     //Risultati
     console.log(`La somma è: ${somma}`);
     console.log(`Il risultato è: ${verifica}`);
-    console.log(resVincitore);
+
+    // Determina chi vince
+    if (sceltaGiocatore === verifica) {
+        console.log("Il giocatore Uno vince la partita");
+    } else {
+        console.log("Il computer vince la partita");
+    }
+
+
 }
 
 //Inizio gioco
